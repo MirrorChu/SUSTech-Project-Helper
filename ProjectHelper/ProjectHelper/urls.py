fe1cf1acc38dacbe42363ca8e09e8b95f9750218
Dim Sim <815488003@qq.com>
2020-11-20 23:00:40 +0800

    Update urls.py
    fix urls

--- ProjectHelper/ProjectHelper/urls.py
+++ ProjectHelper/ProjectHelper/urls.py
@@ -22,7 +22,7 @@ from django.views.static import serve
 
 import xadmin
 
-from items.users.views import LoginView,ChangePasswordView,ShowPersonalDataView,UploadFile,DownloadFile,Test
+from items.users.views import LoginView,ChangePasswordView,ShowPersonalDataView,UploadFile,DownloadFile,Test,StudentGetAllProject
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
@@ -36,6 +36,7 @@ urlpatterns = [
     path('uploadfile/', UploadFile.as_view()),
     path('downloadfile/', DownloadFile.as_view()),
     path('test/', Test.as_view()),
+    path('stugetprojects',StudentGetAllProject.as_view())
     # 传入name使得不同子目录也能对应同一个网页，比如path('login2', LoginView.as_view(), name="login")也能对应登录网页
 ]
 # 访问函数view的实现方法可以基于类(class base view)，也可以基于函数(function base view)，而这里选择前者是因为它便于维护
