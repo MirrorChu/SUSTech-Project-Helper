44073a209ab60f922925fb8d75e37a38fb54c5f4
wateryloo <zhang_jia_shu@qq.com>
2020-12-03 21:30:42 +0800

    save current work

---
+++ ProjectHelper/frontend/src/components/CreateProject.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <el-form-item label="Course">
+      <el-select v-model="this.newProjectCourse" placeholder="Course">
+        <el-option v-for="course in newProjectCourseList" :key="course.value" :label="course.label"
+                   :value="course.value"></el-option>
+      </el-select>
+    </el-form-item>
+    <el-form-item label="Project Name">
+      <el-input v-model="newProjectName" placeholer="Project Name" clearable></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-input v-model="newProjectDescription" placeholer="Description." clearable type="textarea"></el-input>
+    </el-form-item>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CreateProject',
+  props: {
+    sid: {
+      type: String,
+      required: true,
+    },
+    pswd: {
+      type: String,
+      required: true,
+    },
+  },
+  created () {
+
+  },
+  data () {
+    return {
+      newProjectCourse: '',
+      newProjectCourseList: [],
+      newProjectName: '',
+      newProjectDescription: '',
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
