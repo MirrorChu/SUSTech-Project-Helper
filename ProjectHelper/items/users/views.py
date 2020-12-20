e20c3f2ae8aecb053573e6db9a6181414aad5444
Dim Sim <815488003@qq.com>
2020-12-20 21:51:36 +0800

    debug

--- ProjectHelper/items/users/views.py
+++ ProjectHelper/items/users/views.py
@@ -1535,7 +1535,7 @@ class TeacherCreateProject(View):
                 introduction = request.POST.get('newProjectDescription')
                 group_size = int(request.POST.get('groupingMaximum'))
                 min_group_size = int(request.POST.get('groupingMinimum'))
-                course_id = int(request.POST.get('newProjectName'))
+                course_id = int(request.POST.get('newProjectNCourse'))
                 ddl = int(request.POST.get('groupingDeadline'))
                 key = request.POST.get('idx')
             else:
